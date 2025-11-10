4cca9d2868d40d99d3dbe9cadf3d2799e5938aa5
chalaAlex <chalaalemayehu1824@gmail.com>
2025-11-10 08:15:24 -0500

    add a library, choose image from gallery, live camera,

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flutter_face_recognition
+
+// add a library, choose image from gallery, live camera, 
--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,10 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+
+import 'HomeScreen.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home:HomeScreen()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  File? _image;
-
-  //Image Picker Object
-  final ImagePicker _picker = ImagePicker();
-
-  // Methods
-  void chooseImage() async{
-    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-  }
-  void captureImage() {}
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            _image != null ? Image.file(_image!) : Icon(Icons.image),
-            ElevatedButton(
-              onPressed: () {
-                chooseImage();
-              },
-              onLongPress: () {
-                captureImage();
-              },
-              child: Text("Choose/Capture"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
